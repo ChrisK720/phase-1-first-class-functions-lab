40be4574a0ff3637ca7581542a282b049fc0a683
Christopher Kirathe <ckirathe2005@gmail.com>
2023-12-19 21:35:34 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+const drivers = [Mo,Antaonia, Paul,James]
+
+const returnFirstTwoDrivers = function(anArray){
+    return anArray(0,1)
+}
+
+const returnLastTwoDrivers = function(array){
+    return indexOf.array(length-2, length-1)
+}
+
+const  selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+    function multipler(fare){
+        fare * integer
+    }
+   return createFareMultiplier(integer)
+}
